a119761575fa3d3b9104c748e72da144a4c4fb3c
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-02-12 20:46:29 +0530

    Update 5. One away.cpp

--- 1. Arrays and Strings/5. One away.cpp
+++ 1. Arrays and Strings/5. One away.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
-#include  <string>
+#include <string>
 using namespace std;
 
 // returns true if s2 can be made identical to s1,
 // by only one modifiction, i.e. insetion, deletion,
 // or replacement in s2.
 bool isOneEditAway(string s1, string s2) {
-	string a = s1.length() >= s2.length() ? s1 : s2;
-	string b = s1.length() <  s2.length() ? s1 : s2;
+	
+	string large = s1.length() >= s2.length() ? s1 : s2;
+	string small = s1.length() <  s2.length() ? s1 : s2;
 
-	int l1 = a.length();
-	int l2 = b.length();
+	int l1 = large.length();
+	int l2 = small.length();
 	
 	if(abs(l1 - l2) > 1) return false;
 	    
-	bool flag = false;
-	for(int i = 0, j = 0; i < l1 && j < l2;) {
-		if(a[i] != b[j]) {
-			if(flag) return false; 
-			flag = true;
+	bool conflict = false;
+	int i = 0, j = 0;
+	
+	while(i < l1 && j < l2) {
+		
+		if(large[i] != small[j]) {
+			
+			if(conflict) return false; 
+			conflict = true;
 			if(l1 == l2) i++, j++;
 			else i++;
+		} 
+		else {
+			i++, j++;
 		}
-		else i++, j++;
 	}
 	return true;
 }
 
+
 int main(void) {
+	
     string s1, s2;
-  
     getline(cin, s1);
     getline(cin, s2);
     
